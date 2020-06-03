fbd18fe42eb87752a7975c1c28a2d170e0a86209
Pat Ausel <TheSheepGuy1@gmail.com>
2020-06-03 23:59:44 +0100

    Create initial project skeleton

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blackjack_CSharp_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
